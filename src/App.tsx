73df1476a8832c36b089b545a9af851065463ec1
Joshua <joshuatanderson1@gmail.com>
2019-09-08 17:54:07 -0500

    changing data

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,6 @@ const App: React.FC = () => {
   const STATS_PATH: string =
     'https://gist.githubusercontent.com/Fil/fa99e877a5698f5fdf0eb0246c86348b/raw/d3761d7e58f9c7f7d2b3c4679ddd65c86c6c3fdb/unemployment201907.csv';
   const GEO_PATH: string = 'https://gist.githubusercontent.com/john-guerra/43c7656821069d00dcbc/raw/be6a6e239cd5b5b803c6e7c2ec405b793a9064dd/Colombia.geo.json'
-  // https://bl.ocks.org/john-guerra/43c7656821069d00dcbc;
 
   const WIDTH: number = 1000;
   const HEIGHT: number = 750;
@@ -51,10 +50,10 @@ const App: React.FC = () => {
 
   const projection = d3.geoMercator()
     .translate([WIDTH / 2, HEIGHT / 2])
-    .scale(2000)
+    // .scale(2000)
     // this isn't the center of the SVG, but of the map's coordinates
     //https://bl.ocks.org/john-guerra/43c7656821069d00dcbc 
-    .center([-74, 4.5])
+    // .center([-74, 4.5])
 
   const path = d3.geoPath(projection)
 
